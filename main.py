3743630c9e613bd67aec391ab542d1cbce545614
pompyn <pompyn@live.com>
2021-11-30 13:26:26 -0600

    removed invalid statement

--- main.py
+++ main.py
@@ -29,27 +29,28 @@ import random
 choice = input(f"Choose rock, paper or scissors. For rock type 0, for paper type 1 or for scissors type 2\n")
 player_choice = int(choice)
 computer = random.randint(0, 2)
-
-
-
+if player_choice > 2:
+    message = input("Invalid Input!! Please enter 0,1, or 2")
 if computer == player_choice:
-    print(f"The computer chose {computer}. You chose {player_choice}")
+    print(f"The computer chose {computer}.")
     print(f"It is a tie")
 elif computer == 0 and player_choice == 1:
-    print(f"The computer chose {computer}. You chose {player_choice}")
-    print(f"You win. {paper} covers rock")
+    print(f"The computer chose {rock}.")
+    print(f"You win. {paper} Paper covers rock")
 elif computer == 0:
     if player_choice == 2:
-        print(f"The computer chose {computer}. You chose {player_choice}")
-        print(f"You lose. {rock}breaks scissors")
+        print(f"The computer chose {rock}.")
+        print(f"You lose. {rock} Rock breaks scissors")
 elif computer == 1:
     if player_choice == 0:
-        print(f"The computer chose {computer}. You chose {player_choice}")
-        print(f"You Lose!! {paper}covers rock")
+        print(f"The computer chose {paper}.")
+        print(f"You Lose!! {paper} Paper covers rock")
     elif player_choice == 2:
-        print(f"The computer chose {computer}. You chose {player_choice}")
-        print(f"You Win!! {scissors} cut paper")
+        print(f"The computer chose {paper}.")
+        print(f"You Win!! {scissors} Scissors cut paper")
 if computer == 2:
     if player_choice == 1:
-        print(f"The computer chose {computer}. You chose {player_choice}")
-        print(f"You lose.  {scissors} cut paper")
+        print(f"The computer chose {computer}. ")
+        print(f"You lose.  {scissors} Scissors cut paper")
+
+
